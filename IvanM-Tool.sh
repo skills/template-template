ef8f6e5b5e8fce726d45a544c9e305f7b94d2048
Ivanmsdp <lookzii525@gmail.com>
2024-12-02 04:14:40 -0600

    Create IvanM-Tool.sh

---
+++ IvanM-Tool.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+#Colors
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
+clear
+echo -e "$green IvanM🤯 Instalar Proxy En Telegram Y Twitter En Termux"
+sleep 4
+echo -e "$red Agregar Proxy Socks5"
+sleep 5
+echo "Dirección: 127.0.0.1"
+sleep 3 
+echo "Puerto : 9050"
+sleep 5
+echo -e "$yellow Espera Instalando...🫢"
+sleep 6
+clear
+echo "Instalando...🫢"
+sleep 1
+clear
+echo "Instalando...🫢"
+sleep 1
+clear 
+echo "Instalando...🫢"
+sleep 1
+clear
+echo "Instalando...🫢"
+sleep 1
+clear
+echo "Instalando...🫢"
+sleep 1
+clear
+echo "Instalando...🫢"
+sleep 1
+clear
+echo "Instalando...🫢"
+sleep 1
+clear
+echo "Instalando...🫢"
+sleep 1
+clear
+echo "Instalando...🫢"
+sleep 1
+clear
+echo "Instalando...🫢"
+sleep 1
+clear
+echo "Instalando...🫢"
+sleep 1
+clear
+echo "Instalando...🫢"
+sleep 1
+clear 
+echo "Instalando...🫢"
+sleep 1
+clear
+echo "Instalando...🫢"
+sleep 1
+clear
+echo "Instalando...🫢"
+sleep 1
+clear
+echo "Instalando...🫢"
+sleep 1
+clear
+echo "Instalando...🫢"
+sleep 1
+clear
+echo "Instalando...🫢"
+sleep 1
+clear
+echo "Instalando...🫢"
+sleep 1
+clear
+echo "Instalando...🫢"
+sleep 1
+clear
+apt update
+apt upgrade -y
+apt install figlet
+clear 
+figlet -f banner wait
+sleep 3
+figlet -f digital start
+echo "Instalando...🫢"
+sleep 2
+apt install tor privoxy zsh wget git -y
+clear
+figlet wait 
+sleep 3
+sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+##echo “type ‘zsh’ then press enter”
+chsh -s zsh
+cd ~
+echo clear >> .zshrc
+echo figlet -f big WELCOME >> .zshrc
+echo echo "Configurar El Proxy Calcetines5 En Telegrama 127.0.0.1:9050" >> .zshrc
+echo sleep 3 >> .zshrc
+echo echo "Configurar El Proxy Http En Twitter 127.0.0.1:8118" >> .zshrc
+echo echo "Si No, Conéctese A Tor, Establezca El Nombre De Usuario Y La Contraseña = 1" >> .zshrc
+echo sleep 5 >> .zshrc
+echo echo code by init__0 >> .zshrc
+echo sleep 4 >> .zshrc
+echo figlet -f digital wait to run >> .zshrc
+echo sleep 5 >> .zshrc
+echo clear >> .zshrc
+echo figlet -f standard Tor >> .zshrc
+echo sleep 1 >> .zshrc
+echo clear >> .zshrc
+echo figlet -f standard tOr >> .zshrc
+echo sleep 1 >> .zshrc
+echo clear >> .zshrc
+echo figlet -f standard toR >> .zshrc
+echo sleep 1 >> .zshrc
+echo clear >> .zshrc
+echo figlet -f standard Tor >> .zshrc
+echo sleep 1 >> .zshrc
+echo clear >> .zshrc
+echo figlet -f standard tOr >> .zshrc
+echo sleep 1 >> .zshrc
+echo clear >> .zshrc
+echo figlet -f standard toR >> .zshrc
+echo sleep 1 >> .zshrc
+echo clear >> .zshrc
+echo figlet -f big run TOR >> .zshrc
+echo sleep 5 >> .zshrc
+echo tor --HTTPTunnelPort 8118 >> .zshrc
+echo "exit" >> .zshrc
+clear
+figlet -f big DONE
+echo -e "$blue set socks5 proxy in telegram 127.0.0.1:9050"
+echo -e "$green set http proxy in twitter 127.0.0.1:8118"
+echo -e "$red type zsh then close termux and run again $Cafe"
+sleep 3
+chsh
