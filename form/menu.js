6ffaa388e006d965bf49b341959ae412f2ad41d9
tachikoma3 <ma03.trickmaster@gmail.com>
2025-02-20 10:44:15 +0900

    pull

--- form/menu.js
+++ form/menu.js
@@ -1,3 +1,18 @@
 $('.openbtn').on('click',function(){
     $('.nav, .openbtn').toggleClass("open");
   });
+
+   // 📌 最初はメニューを非表示
+   menuBtn.style.opacity = "0";
+   menuBtn.style.pointerEvents = "none";
+
+   // 📌 スクロールしたらボタンを表示（200px以上）
+   window.addEventListener("scroll", function () {
+       if (window.scrollY > 200) {
+           menuBtn.style.opacity = "1";
+           menuBtn.style.pointerEvents = "auto";
+       } else {
+           menuBtn.style.opacity = "0";
+           menuBtn.style.pointerEvents = "none";
+       }
+   });
